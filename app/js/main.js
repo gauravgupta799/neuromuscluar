58f27a9556327c5657e8da93f9e0d6158b14484b
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-05-21 16:52:54 +0530

    menu-toggle-done

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,17 @@
+// const loader = document.getElementsByClassName("pre-loader");
 const header = document.querySelector(".header");
 const formGroup = document.querySelectorAll(".form__group");
-// const loader = document.getElementsByClassName("pre-loader");
+const hamburgerBtn = document.querySelector(".hamburger-wrapper");
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".header-wrapper--mobile");
+const overlay = document.querySelector(".overlay")
 
 
 // window.addEventListener("load",()=>{
 //   loader.style.display = "none";
 // })
 
+// Sticky header
 window.addEventListener("scroll", () => {
     if (window.scrollY > 10) {
       header.classList.add("sticky");
@@ -15,6 +20,25 @@ window.addEventListener("scroll", () => {
     }
   });
 
+
+// Toggle Menu
+let showMenu = false;
+hamburgerBtn.addEventListener('click', toggleMenu);
+function toggleMenu(){
+  if(!showMenu) {
+    hamburger.classList.add('is-open');
+    mobileMenu.classList.add('is-open');
+    overlay.classList.add('is-active');
+    showMenu = true;
+  }else{
+    hamburger.classList.remove('is-open');
+    mobileMenu.classList.remove('is-open');
+    overlay.classList.remove('is-active');
+    showMenu = false;
+  }
+}
+
+// Form group
 formGroup.forEach((item)=>{
   item.addEventListener("focusin", ()=>{
     item.classList.add("isBorder");
